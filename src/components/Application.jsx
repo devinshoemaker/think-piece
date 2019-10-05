6b1d49c5ee27ff79e2eb909b24746bb83fe3e966
Devin Shoemaker <devinshoe@gmail.com>
2019-10-05 13:38:12 -0500

    Application: display authentication component

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import { firestore } from '../firebase';
 import Posts from './Posts';
+import Authentication from './Authentication';
 import { collectIdsAndDocs } from '../utilities';
 
 class Application extends Component {
   state = {
     posts: [],
+    user: null
   };
 
   unsubscribe = null;
@@ -23,11 +25,12 @@ class Application extends Component {
   }
 
   render() {
-    const { posts } = this.state;
+    const { posts, user } = this.state;
 
     return (
       <main className="Application">
         <h1>Think Piece</h1>
+        <Authentication user={user} />
         <Posts posts={posts} />
       </main>
     );
